d740eb06357eaa63f0e9d5cbff5bfdbf1d6de71a
Siddhant Gupta <siddhantgupta@Siddhants-Mac-mini.local>
2025-07-27 14:40:21 +0530

    added serializers

--- api/views.py
+++ api/views.py
@@ -7,20 +7,30 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 # Create your views here.
-@api_view(['GET'])
-def studentsView(req):
-    students=Student.objects.all()
+#def studentsView(req):
+#    students=Student.objects.all()
     ### Without using serializer
     #print(students)          # this will give err as it is not a list so we have to use serializer or do manual serialization
     #students_list=list(students.values())
 
     ### Using Serializer
 
-    def studentsView(request):
-        if request.method == 'GET':
-            students=Student.objects.all()
-            serializer= StudentSerializer(students,many=True)
-            return Response(serializer.data,status=status.HTTP_200_OK)
+@api_view(['GET','POST'])
+def studentsView(request):
+    if request.method == 'GET':
+        students=Student.objects.all()
+        serializer= StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     ### Without Serializer
     # return JsonResponse(students_list, safe=False)
+
+
+    
